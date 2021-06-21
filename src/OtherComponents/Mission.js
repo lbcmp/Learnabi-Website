f6102b0518ce20091c4cdb7a639264f37ed0c3ec
Leah Benitez <benteriordesign@gmail.com>
2021-06-21 16:19:05 -0400

    Created and style nav bar

---
+++ src/OtherComponents/Mission.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Mission extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Mission;
---
+++ src/OtherComponents/Navigation.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
+
+class Navigation extends Component {
+
+    openMenu = () => {
+        let navLinks = document.getElementById("nav-links");
+        navLinks.classList.toggle('active');
+    }
+
+    render() {
+        return (
+            <div>
+                <nav>
+                    <h1>learnabi</h1>
+
+                    <div id="nav-links">
+                        <ul>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="/mission">Our Mission</Link></li>
+                            <li><Link to="/aboutUs">About Us</Link></li>
+                            <li><Link to="/ourTeam">Our Team</Link></li>
+                            <li><Link to="/contact">Contact Us</Link></li>
+                        </ul>
+                    </div>
+
+                    <div className="social-icons">
+                        <a href="https://www.instagram.com/learnabi/" target="_blank" rel="noreferrer">
+                            <i className="fa fa-instagram"></i>
+                        </a>
+
+                        <a href="https://twitter.com/Learnabi" target="_blank" rel="noreferrer">
+                            <i className="fa fa-twitter"></i>
+                        </a>
+                    </div>
+
+                    <div id="span" onClick={this.openMenu}>
+                        <div id="span-bar"></div>
+                        <div id="span-bar"></div>
+                        <div id="span-bar"></div>
+                    </div>
+                </nav>
+
+            </div>
+        )
+    }
+}
+
+export default Navigation;
