4550a6c2553374d45642419dfb91c4c44fe03cfe
Braulio <braumanhattan@gmail.com>
2021-06-21 15:20:33 -0400

    Removed <HashRouter>

--- src/App.js
+++ src/App.js
@@ -1,30 +1,27 @@
-import React from 'react';
-import './App.css';
-import {HashRouter, Switch, Route} from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import { HashRouter, Switch, Route } from "react-router-dom";
 
-import Navigation from './OtherComponents/Navigation'
-import Home from './OtherComponents/Home'
-import Mission from './OtherComponents/Mission'
-import AboutUs from './OtherComponents/AboutUs'
-import OurTeam from './OtherComponents/OurTeam'
-import Contact from './OtherComponents/Contact'
+import Navigation from "./OtherComponents/Navigation";
+import Home from "./OtherComponents/Home";
+import Mission from "./OtherComponents/Mission";
+import AboutUs from "./OtherComponents/AboutUs";
+import OurTeam from "./OtherComponents/OurTeam";
+import Contact from "./OtherComponents/Contact";
 
 const App = () => {
   return (
-    <HashRouter>
-
-    <Navigation />
-      
-    <Switch>
-      <Route exact path="/" component={Home}/>
-      <Route path="/mission" component={Mission}/>
-      <Route path="/aboutUs" component={AboutUs}/>
-      <Route path="/ourTeam" component={OurTeam}/>
-      <Route path="/contact" component={Contact} />
-    </Switch>
-
-    </HashRouter>
+    <div>
+      <Navigation />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/mission" component={Mission} />
+        <Route path="/aboutUs" component={AboutUs} />
+        <Route path="/ourTeam" component={OurTeam} />
+        <Route path="/contact" component={Contact} />
+      </Switch>
+    </div>
   );
-}
+};
 
 export default App;
