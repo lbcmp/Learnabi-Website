a4b15a2c628117fc8f33524d6f40c63ee1d986e5
Leah Benitez <benteriordesign@gmail.com>
2021-06-23 14:02:22 -0400

    Added routes to admin pages

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import AboutUs from "./OtherComponents/AboutUs";
 import OurTeam from "./OtherComponents/OurTeam";
 import Contact from "./OtherComponents/Contact";
 import UserProfile from "./components/user/userProfile";
+import AdminContact from "./OtherComponents/Administrator/AdminContact";
+import AdminTeam from "./OtherComponents/Administrator/AdminTeam";
 
 const App = () => {
   return (
@@ -24,6 +26,8 @@ const App = () => {
           <Route path="/aboutUs" component={AboutUs} />
           <Route path="/ourTeam" component={OurTeam} />
           <Route path="/contact" component={Contact} />
+          <Route path="/adminContact" component={AdminContact} />
+          <Route path="/adminTeam" component={AdminTeam} />
         </Switch>
 
       </HashRouter>
